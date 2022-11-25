297632b359cb2f4f122510069cba7d8e5b1c9ccd
Anonymity1234 <57450707+Anonymity1234@users.noreply.github.com>
2022-11-24 19:28:49 -0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f8e5d7821fmshd767843370f2ae8p181e2fjsn0d0b3fe861c1",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d2e26ec8ff2cd17a626cdc5316e166dc";
